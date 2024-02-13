cb4032343b17c539ae7346c1681762e2bc7b85e7
eckrin <bsh51515@naver.com>
2024-02-13 21:56:09 +0900

    240213: project initialize

---
+++ src/test/java/com/eckrin/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eckrin.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
