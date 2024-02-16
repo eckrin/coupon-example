1cd731fce457cce873278c8eb81dc62d882b827a
eckrin <bsh51515@naver.com>
2024-02-16 23:40:51 +0900

    240216: 쿠폰 발급 도중 생기는 데이터 불일치 문제 해결방법
    - consumer에서 topic을 확인하고 쿠폰을 발급하는 과정에서 에러 발생시 실제 쿠폰이 발급되지 않았음에도(kafka) 발급 개수만 올라갈 수 있다(redis).
    - FailedEvent를 별도 저장한 후, Batch를 돌려서 사후 처리해줄 수 있다.

--- consumer/src/main/java/com/eckrin/consumer/consumer/CouponCreatedConsumer.java
+++ consumer/src/main/java/com/eckrin/consumer/consumer/CouponCreatedConsumer.java
@@ -1,8 +1,12 @@
 package com.eckrin.consumer.consumer;
 
 import com.eckrin.consumer.domain.Coupon;
+import com.eckrin.consumer.domain.FailedEvent;
 import com.eckrin.consumer.repository.CouponRepository;
+import com.eckrin.consumer.repository.FailedEventRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -11,9 +15,16 @@ import org.springframework.stereotype.Component;
 public class CouponCreatedConsumer {
 
     private final CouponRepository couponRepository;
+    private final FailedEventRepository failedEventRepository;
+    private final Logger logger = LoggerFactory.getLogger(CouponCreatedConsumer.class);
 
     @KafkaListener(topics = "coupon_create", groupId = "group_1")
     public void listener(Long userId) {
-        couponRepository.save(new Coupon(userId)); // rdb에 쿠폰 저장
+        try {
+            couponRepository.save(new Coupon(userId)); // rdb에 쿠폰 저장
+        } catch (Exception e) { // 발급도중 실패시 FailedEvent 생성
+            logger.error("failed to create coupon::" + userId);
+            failedEventRepository.save(new FailedEvent(userId));
+        }
     }
 }
---
+++ consumer/src/main/java/com/eckrin/consumer/repository/FailedEventRepository.java
@@ -0,0 +1,7 @@
+package com.eckrin.consumer.repository;
+
+import com.eckrin.consumer.domain.FailedEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FailedEventRepository extends JpaRepository<FailedEvent, Long> {
+}
